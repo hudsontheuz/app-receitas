df3b23ead1912fed79cf3078759382fa8bb07eda
hudsontheuz <hudsonknad@gmail.com>
2025-06-10 12:10:44 -0300

    fix(rotas): corrige rota inicial e instala dependências pendentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { PostList } from "./pages/PostList";
 import { PostDetails } from "./pages/PostDetails";
 
@@ -6,6 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Redireciona "/" para "/post" */}
+        <Route path="/" element={<Navigate to="/post" />} />
         <Route path="/post" element={<PostList />} />
         <Route path="/dados/:id" element={<PostDetails />} />
       </Routes>
