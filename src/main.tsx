3b665fb064e4c3ca84fc137b258de9fbfe80b246
hudsontheuz <hudsonknad@gmail.com>
2025-06-10 19:51:42 -0300

    fix(build): corrige caminho do main.tsx no index.html e ajusta base no vite.config.ts para deploy
    Ajusta a configuração de build para funcionar corretamente no Vercel com React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import { theme } from "./theme";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Reseta o estilo base do navegador */}
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // importante para o roteamento funcionar no deploy
+  base: "./",
 });
